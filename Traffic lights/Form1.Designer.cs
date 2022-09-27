5f573e87ee3d1e7bc89e6147c3934e3edfa7dd44
MDNET-Developer <qarabagli.murad@mail.ru>
2022-09-27 22:36:23 +0400

    traffic light windows forms

---
+++ Traffic lights/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Traffic_lights
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 60);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 8);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(366, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 62);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(366, 260);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 64);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("NFS font", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(370, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 36);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("NFS font", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(267, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 72);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ".";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Traffic lights/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Traffic_lights
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Burda form1 -de timer.Enabled=ture deyerek ise saliriq
+            timer1.Enabled = true;
+            //Demeli vaxt baslayaraq ilk proses button1 - i qirmizi etmek ve label2- de qirmizi rengde dayan yazmaq
+            button1.BackColor = Color.Red;
+            label2.Text = "Dayan";
+            label2.ForeColor = Color.Red;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Burda vaxt adinda deyisen verirem ki , bu bir bir artaraq zaman intervalinda dovr etsin
+            //interger deyisenine idxal ederek label1-de gorsenmesini yaziriq
+            int vaxt = int.Parse(label1.Text);
+            vaxt++;
+            label1.Text = vaxt.ToString();
+            //Biz evvelden Form1 daxilinde qirmizi rengi vermisdik. Burada sert olaraq yaziriq ki,eger 30-cu saniye catsa button3 sari olsun
+            if (vaxt == 30)
+            {
+                button3.BackColor = Color.Yellow;
+            }
+            //Sonra 40-ci saniye yaaziriq ve bu prosesden 10 saniye kecir. Hemin 10 saniye muddetinde sari isiq yanir.
+            //Artiq 40-ci saniyede button1 ve button3 iqilari sonur yeni boz rengde olur.Button4 yasil reng olur ve label2-de davam et yazilir 
+            //label2-deki davam et duymesi yasil rengde verilir
+            if (vaxt == 40)
+            {
+                button1.BackColor = Color.Gray;
+                button3.BackColor = Color.Gray;
+                button4.BackColor = Color.Green;
+                label2.Text = "Davam Et";
+                label2.ForeColor = Color.Green;
+            }
+            //Belelikle yasil rengin yanmasina 30 saniye verilir qirmizi rengde olugu kimi
+            // sonra dovr yeniden baslayir. Ona gorede vaxt=0 yaziriq ki , yeniden hemin proses yazilsin
+            //Yeniden button1 qirmizi yaziriq, buton3 -e sari yazmaq ehtiyyac yoxdur cunki yuxarida qeyd olunub ki, 30-cu saniye intervalinda olsun
+            //button4-e ise gray deyilir
+            // Hemin qaydada lebel2-e dayan yazilir ve qirmizi rengde gorsenir
+            // Asagidaki proses verilen emrin tekrar ve tekar emel edilmesini sazlayir
+            if (vaxt == 70)
+            {
+                vaxt = 0;
+                label1.Text = vaxt.ToString();
+                button1.BackColor = Color.Red;
+                label2.Text = "Dayan";
+                label2.ForeColor = Color.Red;
+                
+                button4.BackColor = Color.Gray;
+
+            }
+        }
+    }
+}
